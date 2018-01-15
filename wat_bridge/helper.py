510d8b424118640834d0c26bedba8e9db3968d6b
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-01-15 21:13:00 +0530

    @balasankarc issue fix

--- wat_bridge/helper.py
+++ wat_bridge/helper.py
@@ -216,4 +216,8 @@ def safe_cast(val, to_type, default=None):
         return default
 
 def wa_id_to_name(val):
-    return hashlib.md5(str(val).encode('utf-8')).hexdigest()
+    if val :
+        return hashlib.md5(str(val).encode('utf-8')).hexdigest()
+    else :
+        return None
+
