22dbbe0ce75071f555d3bba2fc9f8b1b1d2bfcc3
Fabian Wunsch <fabian@uriah.heep.sax.de>
2019-11-13 16:07:06 +0100

    Changes at shellscripts

--- kill.sh
+++ kill.sh
@@ -3,7 +3,5 @@
     do
       kill -9 ${j}
     done
-    sh CleanUpDownLoadS.sh
-    sh UploadToTG.sh
     rm log.pid
     rm log.txt
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd $PWD/WhatsApp-Telegram-Bridge/
 WAT_CONF=config.conf python3 watbridge.py >> log.txt 2>&1 & echo $! >> log.pid
