627b881e9ba953ed71feb717fad832735c6820ef
Fabian Wunsch <fabian@uriah.heep.sax.de>
2020-05-06 20:56:11 +0200

    Added pipes

---
+++ Utilities/Pipes.py
@@ -0,0 +1,18 @@
+from os import pipe, write, fdopen
+from typing import Dict
+
+
+class Pipes:
+    pipes: Dict[str, int] = {}
+
+    def __init__(self, descriptor: str):
+        r, w = pipe()
+        self.read_pipe = fdopen(r)
+        self.pipes[descriptor] = w
+
+    def send(self, descriptor: str, data: str):
+        data += '\n'
+        write(self.pipes[descriptor], data.encode())
+
+    def receive(self) -> str:
+        return self.read_pipe.readline()[:-1]
