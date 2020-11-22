cab02dc5d3bf94b36f265a17a88b2c25db65c3dc
Fabian Wunsch <fabian@uriah.heep.sax.de>
2020-11-22 22:52:25 +0100

    Removed get_pipe from Redirectable.py

--- DBModels/Redirectable.py
+++ DBModels/Redirectable.py
@@ -3,13 +3,8 @@ from __future__ import annotations
 from persistent import Persistent
 from persistent.mapping import PersistentMapping
 
-from Utilities.typings import WaMessageId, TgMessageId
-
 
 class Redirectable(Persistent):
     def __init__(self):
         super().__init__()
         self.redirect: PersistentMapping[Redirectable, Redirectable] = PersistentMapping()
-
-    def get_pipe(self, msg_id: [WaMessageId, TgMessageId]) -> str:
-        return f'Message\x1f{msg_id}'
